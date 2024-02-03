8f345f5fe3cda17ec96d9fd588d79eaae7396d53
Amin-ElGazzar <aminelgazzar0@gmail.com>
2024-02-03 23:46:39 +0200

    Cake Shop

---
+++ main.js
@@ -0,0 +1,32 @@
+window.onscroll = function () {
+  if (window.scrollY > 300) {
+    buttonup.style.display = "block";
+  } else {
+    buttonup.style.display = "none";
+  }
+};
+
+let buttonup = document.querySelector(".up");
+
+buttonup.onclick = function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
+
+let items = document.querySelectorAll(".carousel .carousel-item");
+
+items.forEach((el) => {
+  const minPerSlide = 6;
+  let next = el.nextElementSibling;
+  for (var i = 1; i < minPerSlide; i++) {
+    if (!next) {
+      // wrap carousel by using first child
+      next = items[0];
+    }
+    let cloneChild = next.cloneNode(true);
+    el.appendChild(cloneChild.children[0]);
+    next = next.nextElementSibling;
+  }
+});
